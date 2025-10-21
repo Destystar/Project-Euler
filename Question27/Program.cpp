69de608b24b09a43dd17f9d1c6e00630078aa734
Destystar <95242511+Destystar@users.noreply.github.com>
2025-10-21 20:33:28 +0100

    Q27 fixed

--- Question27/Program.cpp
+++ Question27/Program.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 bool isPrime(int a){
-    for (int i = 2; i <= sqrt(a); i++){
+    if (a <= 1) return false;
+    if (a == 2) return true;
+    if (a % 2 == 0) return false;
+    
+    for (int i = 3; i <= sqrt(a); i += 2){
         if (a % i == 0){
             return false;
         }
@@ -13,27 +17,31 @@ bool isPrime(int a){
 
 int main(){
     int primesMax = 0;
+    int bestA = 0, bestB = 0;
 
-    for (int i = -999; i < 1000; i++){
-        bool temp = true;
-        for (int j = -1000; j <= 1000; j++){
+    for (int a = -999; a < 1000; a++){
+        for (int b = -1000; b <= 1000; b++){
             int count = 0;
             int n = 0;
-            while (temp){
-                int temp2 = n*n + i*n + j;
-                if (isPrime(temp2)){
+            while (true){
+                int result = n*n + a*n + b;
+                if (isPrime(result)){
                     count++;
                     n++;
                 }
                 else{
-                    temp = false;
+                    break;
                 }
             }
             if (count > primesMax){
                 primesMax = count;
+                bestA = a;
+                bestB = b;
             }
         }
     }
-    cout << primesMax << endl;
+    cout << "Maximum consecutive primes: " << primesMax << endl;
+    cout << "Coefficients: a = " << bestA << ", b = " << bestB << endl;
+    cout << "Product: " << bestA * bestB << endl;
     return 0;
 }
