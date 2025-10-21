84690d49387235f17419506473345c60cfe226b6
Destystar <95242511+Destystar@users.noreply.github.com>
2025-10-21 20:52:51 +0100

    Q28

---
+++ Question28/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+BigInteger sum = 1; 
+
+for (int n = 3; n <= 1001; n += 2)
+{
+    BigInteger nSquared = (BigInteger)n * n;
+    sum += nSquared; 
+    sum += nSquared - (n - 1);   
+    sum += nSquared - 2 * (n - 1); 
+    sum += nSquared - 3 * (n - 1); 
+}
+
+Console.WriteLine(sum);
